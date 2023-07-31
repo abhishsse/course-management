50220687bfd7373f7ceece19beeda4693764d666
abhishsse <138072787+abhishsse@users.noreply.github.com>
2023-07-31 07:43:00 -0400

    API-Key

---
+++ API-Key-Att/src/main/java/com/in/att/ApiKeyAttApplication.java
@@ -0,0 +1,13 @@
+package com.in.att;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiKeyAttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiKeyAttApplication.class, args);
+	}
+
+}
---
+++ API-Key-Att/src/test/java/com/in/att/ApiKeyAttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in.att;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiKeyAttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
